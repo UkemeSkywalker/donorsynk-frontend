cfdcc448db1dfc95a427cd4834073e4b7e0ddabc
Ukeme David Eseme <skywalkershadow9@gmail.com>
2023-09-08 12:01:25 +0000

    component folder

--- donorsynk/src/app/page.js
+++ donorsynk/src/app/page.js
@@ -1,6 +1,8 @@
+import NavBar from './components/navbar'
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <NavBar />
       <h1>Welcome</h1>
     </main>
   )
