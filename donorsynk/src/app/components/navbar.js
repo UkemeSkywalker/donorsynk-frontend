40dbd9779bb7a0e458118b0dbd503b90ec5ce420
Ukeme David Eseme <skywalkershadow9@gmail.com>
2023-09-08 12:41:27 +0000

    navbar

--- donorsynk/src/app/components/navbar.js
+++ donorsynk/src/app/components/navbar.js
@@ -1,21 +1,19 @@
-// components/NavBar.js
-
 import Link from 'next/link';
+import Image from 'next/image';
 
 const NavBar = () => {
   return (
-    <nav>
-      <ul>
-        <li>
-          <Link href="/">Home</Link>
-        </li>
-        <li>
-          <Link href="/about">About</Link>
-        </li>
-        <li>
-          <Link href="/contact">Contact</Link>
-        </li>
-      </ul>
+    <nav className="bg-white p-4  w-full">
+        <div className='mx-10 flex justify-between items-center'>
+            <Link href="/">
+                <Image 
+                    src="/images/logo.png"
+                    width={40}
+                    height={40}
+                />
+            </Link>
+            <button className="connect-wallet text-white px-4 py-2 rounded-md">Connect Wallet</button>
+        </div>
     </nav>
   );
 };
--- donorsynk/src/app/page.js
+++ donorsynk/src/app/page.js
@@ -1,7 +1,8 @@
 import NavBar from './components/navbar'
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    
+    <main className="flex min-h-screen flex-col items-center justify-between">
       <NavBar />
       <h1>Welcome</h1>
     </main>
